e0d314fc84e3d7892f8b58dab79a2cd309fb022d
riffca <riffca@yandex.ru>
2017-09-28 12:31:35 +0300

    set prod routes

--- app/Routes.jsx
+++ app/Routes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Route, IndexRoute } from "react-router/es";
 import willTransitionTo from "./routerTransition";
-import App from "./App";
+import App from "./Trusty";
 
 /*
 * Electron does not support async loading of components via System.import,
@@ -26,10 +26,41 @@ function errorLoading(err) {
 
 const routes = (
     <Route path="/" component={App} onEnter={willTransitionTo}>
+        {/*Trusty Routes*/}
+        
         <IndexRoute getComponent={(location, cb) => {
             System.import("components/Dashboard/DashboardContainer").then(loadRoute(cb)).catch(errorLoading);
         }}/>
-        <Route path="/auth/:data" component={Auth}/>
+
+        <Route path="/signup" getComponent={(location, cb) => {
+            System.import("components/Trusty/Account/CreateAccount").then(loadRoute(cb)).catch(errorLoading);
+        }}/>
+        <Route path="/landing" getComponent={(location, cb) => {
+            System.import("components/Trusty/Landing/Landing").then(loadRoute(cb)).catch(errorLoading);
+        }}/>
+
+        <Route path="/home" getComponent={(location, cb) => {
+            System.import("components/Trusty/Account/AccountPage").then(loadRoute(cb)).catch(errorLoading);
+        }}>
+            <IndexRoute getComponent={(location, cb) => {
+                System.import("components/Trusty/Account/AccountOverview").then(loadRoute(cb)).catch(errorLoading);
+            }}/>
+            <Route path="/overview" getComponent={(location, cb) => {
+                System.import("components/Trusty/Account/AccountOverview").then(loadRoute(cb)).catch(errorLoading);
+            }}/>
+        </Route>
+
+        <Route path="/withdraw" getComponent={(location, cb) => {
+            System.import("components/Trusty/Account/Withdraw").then(loadRoute(cb)).catch(errorLoading);
+        }}/>
+
+        <Route path="/deposit" getComponent={(location, cb) => {
+            System.import("components/Trusty/Account/Deposit").then(loadRoute(cb)).catch(errorLoading);
+        }}/>
+
+        {/*Trusty Routes*/}
+
+
         <Route path="/dashboard" getComponent={(location, cb) => {
             System.import("components/Dashboard/DashboardContainer").then(loadRoute(cb)).catch(errorLoading);
         }}/>
