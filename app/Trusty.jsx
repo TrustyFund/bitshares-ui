7828bcead372f51f7ec686007cc967d5af99f7ac
riffca <riffca@yandex.ru>
2017-10-31 21:57:12 +0300

    change background

--- app/Trusty.jsx
+++ app/Trusty.jsx
@@ -26,8 +26,8 @@ import Icon from "components/Icon/Icon"
 
 
 /* pixel perfect helper */
-import 'components/Trusty/pixel-glass'
-import 'assets/stylesheets/trusty/components/pixel-glass.scss'
+// import 'components/Trusty/pixel-glass'
+// import 'assets/stylesheets/trusty/components/pixel-glass.scss'
 
 import {dispatcher} from 'components/Trusty/utils'
 const user_agent = navigator.userAgent.toLowerCase();
@@ -239,8 +239,9 @@ class Trusty extends React.Component {
         return (
             <div style={{backgroundColor: !this.state.theme ? "#2a2a2a" : null}} className={this.state.theme}>
                 
-                <img src={ require("assets/stylesheets/trusty/texture_mob_bgr.png")} className="trusty_fixed_background _mob"/>
+                {/*<img src={ require("assets/stylesheets/trusty/texture_mob_bgr.png")} className="trusty_fixed_background _mob"/>*/}
                 
+                <div className="trusty_fixed_background_div _mob"/>
 
                 <div id="content-wrapper" className="trusty-wrapper">
                     {content}
