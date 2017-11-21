2709a499ac8089df9438060d3377cd6d597408a6
Anton <anlopan@gmail.com>
2017-11-22 02:50:30 +0300

    change prod routes

--- app/Routes.jsx
+++ app/Routes.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 import { Route, IndexRoute, Redirect } from "react-router/es";
 import willTransitionTo from "./routerTransition";
-import App from "./Trusty";
+import App from "./AppContainer";
+import Trusty from "./Trusty";
 
 /*
 * Electron does not support async loading of components via System.import,
@@ -27,47 +28,53 @@ function errorLoading(err) {
 const routes = (
     <Route path="/" component={App} onEnter={willTransitionTo}>
         {/*Trusty Routes*/}
+        <IndexRoute component={Landing}/>
 
-        <Route path="/signup" getComponent={(location, cb) => {
-            System.import("components/Trusty/Account/CreateAccount").then(loadRoute(cb)).catch(errorLoading);
-        }}/>
-        <Route path="/landing" getComponent={(location, cb) => {
-            System.import("components/Trusty/Landing/Landing").then(loadRoute(cb)).catch(errorLoading);
-        }}/>
+        <Route path="/home" component={Trusty} onEnter={willTransitionTo}>
 
-        <Route path="/home" getComponent={(location, cb) => {
-            System.import("components/Trusty/Account/AccountPage").then(loadRoute(cb)).catch(errorLoading);
-        }}/>
+            <IndexRoute getComponent={(location, cb) => {
+                System.import("components/Trusty/Account/AccountPage").then(loadRoute(cb)).catch(errorLoading);
+            }}/>
 
-        <Route path="/withdraw" getComponent={(location, cb) => {
-            System.import("components/Trusty/Account/Withdraw").then(loadRoute(cb)).catch(errorLoading);
-        }}/>
+            <Route path="/signup" getComponent={(location, cb) => {
+                System.import("components/Trusty/Account/CreateAccount").then(loadRoute(cb)).catch(errorLoading);
+            }}/>
+            <Route path="/landing" getComponent={(location, cb) => {
+                System.import("components/Trusty/Landing/Landing").then(loadRoute(cb)).catch(errorLoading);
+            }}/>
 
-        <Route path="/deposit" getComponent={(location, cb) => {
-            System.import("components/Trusty/Account/Deposit").then(loadRoute(cb)).catch(errorLoading);
-        }}/>
+            
 
-        <Route path="/create-wallet-brainkey" getComponent={(location, cb) => {
-            System.import("components/Trusty/Wallet/WalletCreate").then(loadRoute(cb, "CreateWalletFromBrainkey")).catch(errorLoading);
-        }}/>
+            <Route path="/withdraw" getComponent={(location, cb) => {
+                System.import("components/Trusty/Account/Withdraw").then(loadRoute(cb)).catch(errorLoading);
+            }}/>
 
+            <Route path="/deposit" getComponent={(location, cb) => {
+                System.import("components/Trusty/Account/Deposit").then(loadRoute(cb)).catch(errorLoading);
+            }}/>
 
-        <Route path="/terms-of-use" getComponent={(location, cb) => {
-            System.import("components/Trusty/Account/TermsOfUse").then(loadRoute(cb)).catch(errorLoading);
-        }}/>
-
-        <Route path="/manage" getComponent={(location, cb) => {
-            System.import("components/Trusty/Portfolio/Manage").then(loadRoute(cb)).catch(errorLoading);
-        }}/>
-
-        <Route path="/unlock" getComponent={(location, cb) => {
-            System.import("components/Trusty/UnlockAccount").then(loadRoute(cb)).catch(errorLoading);
-        }}/>
+            <Route path="/create-wallet-brainkey" getComponent={(location, cb) => {
+                System.import("components/Trusty/Wallet/WalletCreate").then(loadRoute(cb, "CreateWalletFromBrainkey")).catch(errorLoading);
+            }}/>
 
 
-        <Route path="/init-error" getComponent={(location, cb) => {
-            System.import("components/InitError").then(loadRoute(cb)).catch(errorLoading);
-        }}/>
+            <Route path="/terms-of-use" getComponent={(location, cb) => {
+                System.import("components/Trusty/Account/TermsOfUse").then(loadRoute(cb)).catch(errorLoading);
+            }}/>
+
+            <Route path="/manage" getComponent={(location, cb) => {
+                System.import("components/Trusty/Portfolio/Manage").then(loadRoute(cb)).catch(errorLoading);
+            }}/>
+
+            <Route path="/unlock" getComponent={(location, cb) => {
+                System.import("components/Trusty/UnlockAccount").then(loadRoute(cb)).catch(errorLoading);
+            }}/>
+
+
+            <Route path="/init-error" getComponent={(location, cb) => {
+                System.import("components/InitError").then(loadRoute(cb)).catch(errorLoading);
+            }}/>
+        </Route>
 
 
     </Route>
