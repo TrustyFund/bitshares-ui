63fb3995d0dbad16d423705fd2213e4cab360a59
Anton <anlopan@gmail.com>
2017-12-26 08:21:43 +0300

    Remove useless deposit code

--- app/components/Trusty/Account/Deposit.jsx
+++ app/components/Trusty/Account/Deposit.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import DepositWithdraw from "./Withdraw"
-import DepositFiat from "../Cryptobot/Deposit";
 
 class Deposit extends React.Component {
 	constructor(){
@@ -11,7 +10,6 @@ class Deposit extends React.Component {
       return (
         <div className="trusty_deposit" style={{paddingTop: "10px 2rem 0 2rem"}}>
           <DepositWithdraw router={this.props.router} location={this.props.location} deposit={true} />
-          {/*<DepositFiat currency="RUB" method="SBERBANK" />*/} 
         </div>
       )
   }
